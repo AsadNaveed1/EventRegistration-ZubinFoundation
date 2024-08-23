87b9e016e7115e0065952996c8049fa05d60a118
JoshC <josh.chau.github@gmail.com>
2024-08-23 19:19:24 +0800

    update

--- Server/barebone/settings.py
+++ Server/barebone/settings.py
@@ -49,8 +49,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'zubin_auth',
+
+
+    ## For CORS 
+    'corsheaders',
 ]
 
+
+## For CORS 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # 允许的前端地址
+
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -59,6 +69,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    ## For CORS 
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'barebone.urls'
