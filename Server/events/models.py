50111e58fac30eff168604433e4f17bbbc7eead6
CodingNovi <113091320+CodingNovi@users.noreply.github.com>
2024-08-25 00:56:06 +0800

    events model

--- Server/events/models.py
+++ Server/events/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-
 from django.contrib.postgres.fields import ArrayField
 from user.models import User
 
+class Module(models.Model):
+    module_id = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)  # Added max_length
+    title = models.CharField(max_length=255)
+    status = models.BooleanField()
+    image = models.CharField(max_length=255)  # Added max_length
+
+    def __str__(self):
+        return self.title
 
 class Event(models.Model):
     image_src = models.URLField()
@@ -11,23 +19,13 @@ class Event(models.Model):
     time = models.TimeField()
     location = models.CharField(max_length=255)
     eventid = models.CharField(max_length=255, unique=True)
-    required_modules =  model
-    volunteers = models
+    required_modules = models.ManyToManyField(Module, blank=True)
+
+    # Specify unique related names for each ManyToManyField
+    volunteers = models.ManyToManyField(User, blank=True, related_name='volunteers')  
+    participants = models.ManyToManyField(User, blank=True, related_name='participants')  
+    capacity = models.IntegerField(null=True)  
+    waitList = models.ManyToManyField(User, blank=True, related_name='waitlist')  
 
     def __str__(self):
-        return self.eventid
-    
-
-
-
-
-# Define the field first
-many_to_many_events_field = models.ManyToManyField(Event, related_name='users')
-many_to_many_users_field = models.ManyToManyField(User, related_name='events')
-
-# Then add it to the model
-# User.add_to_class('registered_events', many_to_many_events_field)
-
-
-
-Event.add_to_class('registered_users', many_to_many_users_field)
+        return f"{self.title} ({self.eventid})"
