c225e38526329b1182ea21ccae540c85c4b29310
Dilnaz AUSHAKHIMOVA <dilnaz.aushakhimova@gmail.com>
2024-08-25 18:25:59 +0800

    Update ChatBot.jsx

--- ChatBot/ChatBot.jsx
+++ ChatBot/ChatBot.jsx
@@ -1,43 +1,55 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const DialogflowMessenger = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js';
-    script.async = true;
-
-    script.onload = () => {
-      window.dfMessengerSettings = {
-        oauthClientID: '590459718731-npr85ffmig06bkur9e7tm0c5kbne6ssu.apps.googleusercontent.com',
-        projectID: 'ai-tester-433403',
-        agentID: '7ab3816e-89e5-4caa-aa5e-02b1d6228d74',
-        languageCode: 'en',
-        maxQueryLength: -1,
-        chatTitle: 'Volunteer_helper',
-        enableHistory: true,
-        defaultHeight: 500,
-        defaultWidth: 400
-      };
-
-      const dfMessenger = document.createElement('df-messenger');
-      dfMessenger.setAttribute('id', 'df-messenger-container');
-
-      document.body.appendChild(dfMessenger);
-    };
-
-    document.head.appendChild(script);
+  React.useEffect(() => {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js';
+    scriptElement.async = true;
+    document.body.appendChild(scriptElement);
 
     return () => {
-      document.head.removeChild(script);
-      window.dfMessengerSettings = undefined;
-      const dfMessenger = document.getElementById('df-messenger-container');
-      if (dfMessenger) {
-        document.body.removeChild(dfMessenger);
-      }
+      document.body.removeChild(scriptElement);
     };
   }, []);
 
-  return null;
+  return (
+    <>
+      <MessengerContainer>
+        <df-messenger
+          project-id="ai-tester-433403"
+          agent-id="7ab3816e-89e5-4caa-aa5e-02b1d6228d74"
+          language-code="en"
+          max-query-length="-1"
+        >
+          <df-messenger-chat-bubble chat-title="AI assistant" />
+        </df-messenger>
+      </MessengerContainer>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default DialogflowMessenger;
+
+const MessengerContainer = styled.div`
+  z-index: 999;
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+
+  --df-messenger-font-color: #000;
+  --df-messenger-font-family: Lato;
+  --df-messenger-chat-background: #FEF8E6;
+  --df-messenger-message-user-background: #C6DAFC;
+  --df-messenger-message-bot-background: #fff;
+`;
+
+const GlobalStyles = createGlobalStyle`
+  df-messenger {
+    z-index: 999;
+    position: fixed;
+    bottom: 16px;
+    right: 16px;
+  }
+`;
